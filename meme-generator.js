c5660896aa85ce2dd5976f69dda65078e3d48a71
Andre Pautin <andre.pautin@gmail.com>
2021-03-10 15:14:10 -0800

    html format and js intro

---
+++ meme-generator.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     let memeData = document.getElementById("meme-data");
+//     memeData.addEventListener("submit", function(e) {
+//         e.preventDefault();
+//     });
+// });
