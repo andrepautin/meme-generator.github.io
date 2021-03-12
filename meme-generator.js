a6b6aa6daeb20b8230f9109565ed60dc243d6289
Andre Pautin <andre.pautin@gmail.com>
2021-03-12 12:06:31 -0800

    add prevent default and more styling

--- meme-generator.js
+++ meme-generator.js
@@ -37,32 +37,35 @@
 
 function generateMeme() {
     let memeData = document.getElementById("meme-data");
-    let memeList = document.querySelector(".memes");
-    let memeItem = document.createElement("div");
-    memeItem.classList.add("meme");
-
-    let imageURL = document.getElementById("meme-image").value, 
-    src = imageURL, 
-    image = document.createElement("img");
-    image.src = src;
-
-    let memeTop = document.createElement("div");
-    memeTop.classList.add("text", "top");
-    memeTop.innerText = document.getElementById("top-text").value;
-
-    let memeBottom = document.createElement("div");
-    memeBottom.classList.add("text", "bottom");
-    memeBottom.innerText = document.getElementById("bottom-text").value;
-
-    let removeMeme = document.createElement("div");
-    removeMeme.classList.add("delete-mark");
-    removeMeme.innerText = "X";
-    removeMeme.style.color = "red";
-
-    memeList.appendChild(memeItem);
-    memeItem.appendChild(image);
-    memeItem.appendChild(memeTop);
-    memeItem.appendChild(memeBottom);
-    memeItem.appendChild(removeMeme);
-    memeData.reset();
+    memeData.addEventListener("submit", function(e) {
+        e.preventDefault;
+        let memeList = document.querySelector(".memes");
+        let memeItem = document.createElement("div");
+        memeItem.classList.add("meme");
+    
+        let imageURL = document.getElementById("meme-image").value, 
+        src = imageURL, 
+        image = document.createElement("img");
+        image.src = src;
+    
+        let memeTop = document.createElement("div");
+        memeTop.classList.add("text", "top");
+        memeTop.innerText = document.getElementById("top-text").value;
+    
+        let memeBottom = document.createElement("div");
+        memeBottom.classList.add("text", "bottom");
+        memeBottom.innerText = document.getElementById("bottom-text").value;
+    
+        let removeMeme = document.createElement("div");
+        removeMeme.classList.add("delete-mark");
+        removeMeme.innerText = "X";
+        removeMeme.style.color = "red";
+    
+        memeList.appendChild(memeItem);
+        memeItem.appendChild(image);
+        memeItem.appendChild(memeTop);
+        memeItem.appendChild(memeBottom);
+        memeItem.appendChild(removeMeme);
+        memeData.reset();
+    });
 }
